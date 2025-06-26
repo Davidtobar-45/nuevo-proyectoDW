72ef8547598bc8dedef7e26addcb7c872950e03b
David <datobarmi@pucesi.edu.ec>
2025-06-26 18:43:50 -0500

    commit

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,6 +6,13 @@ class Home extends BaseController
 {
     public function index(): string
     {
-        return view('/VistaLogin/index');
+        return view('/VistaLogin/index.php');
+    }
+
+// nuevo metodo de formulario 
+
+    public function MetodoMostrarFormulario2(): string
+    {
+        return view('/VistaFormularios/Formulario1.php');
     }
 }
