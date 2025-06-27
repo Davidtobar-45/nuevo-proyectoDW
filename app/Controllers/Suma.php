8b70b7e0383bc03ee814c2f08943b14069084f95
David <datobarmi@pucesi.edu.ec>
2025-06-26 20:30:46 -0500

    ..

--- app/Controllers/Suma.php
+++ app/Controllers/Suma.php
@@ -12,8 +12,8 @@ class CEjemplo extends BaseController
 
     public function index(): string
     {
-        echo view('/Contenido/Encabesado.php');
-        echo view('/VistaFormulario/Cuerpo.php');
+    $numero1= $this ->request ->getPost('numero 1');
+    $numero1= $this ->request ->getPost('numero 2');
     }
 
 }
