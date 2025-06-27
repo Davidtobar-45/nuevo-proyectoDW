e1dfa86a48553b27854952296c4ff4490b47ae2d
David <datobarmi@pucesi.edu.ec>
2025-06-26 20:56:26 -0500

    commit

--- app/Views/VistaFormulario/SumaResultado.php
+++ app/Views/VistaFormulario/SumaResultado.php
@@ -6,8 +6,8 @@
     <title>Document</title>
 </head>
 <body>
-    <h2> Resultado </h2>
-    <p><?=$numero1?> + <?=$numero2?> = <?=$resultado?> <p>
-        <a href 
+    <h2>Resultado</h2>
+    <p><?=$numero1?> + <?=$numero2?> = <?=$resultado?> </p>
+    <a href=" <?= base_url('/Suma') ?> ">Redireccionar a la Pagina Suma</a>
 </body>
 </html>
