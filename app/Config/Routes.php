0d8c177c99db4f435562196205a9aa2be763b70c
David <datobarmi@pucesi.edu.ec>
2025-06-26 19:09:05 -0500

    commit

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,5 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+//http://localhost/nuevo-proyectoDW/vista1
+$routes->get('/Vista1', 'Home::MetodoMostrarFormulario2');
