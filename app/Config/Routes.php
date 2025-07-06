ed13563be31f3151e1e4e6496b7a3d4962c46369
“David <datobarmi@pucesi.edu.ec>
2025-07-05 19:27:54 -0500

    Tarea_Semana_9

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -2,17 +2,31 @@
 
 use CodeIgniter\Router\RouteCollection;
 
+// Variable para las rutas
 /**
  * @var RouteCollection $routes
  */
+
+// Ruta principal de la página, muestra la página de inicio
 $routes->get('/', 'Home::index');
+
+// Permite que las rutas se creen automáticamente según el controlador y método
 $routes->setAutoRoute(true);
 
+// Ruta para mostrar la página Z1 en el controlador de zapatos
 $routes->get('Z1', 'ControladorZapatos::Z1');
+
+// Ruta para mostrar la página Z2 en el controlador de zapatos
 $routes->get('Z2', 'ControladorZapatos::Z2');
 
+// Ruta para mostrar la página V1 en el controlador de vestidos
 $routes->get('V1', 'ControladorVestido::V1');
+
+// Ruta para mostrar la página V2 en el controlador de vestidos
 $routes->get('V2', 'ControladorVestido::V2');
 
+// Ruta para mostrar la página C1 en el controlador de carteras
 $routes->get('C1', 'ControladorCartera::C1');
+
+// Ruta para mostrar la página C2 en el controlador de carteras
 $routes->get('C2', 'ControladorCartera::C2');
--- app/Controllers/ControladorZapatos.php
+++ app/Controllers/ControladorZapatos.php
@@ -5,12 +5,12 @@ namespace App\Controllers;
 class ControladorZapatos extends BaseController
 {
     public function Z1(): string
-    {
+    { //carga la vista de zapatos deportivos
         return view('ContenedorZapatos/Deportivos');
     }
 
     public function Z2(): string
-    {
+    { // carga la vista de zapatos casuales
         return view('ContenedorZapatos/Casual');
     }
 }
